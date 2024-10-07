ffc159c264b04d662a7f4060a38b8e3480277fc5
Amaankhan27 <ak2730929@gmail.com>
2024-10-07 15:21:08 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
-
-
-
+        # Mapping of Roman numerals to their integer values
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            # Check if the current value is less than the next value
+            if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]  # Subtract if current is less
+            else:
+                total += roman_map[s[i]]  # Add otherwise
+                
+        return total
